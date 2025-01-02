4bfc512e620732457330e548c1cd14e633a3fd8c
cwwojin <cwwojin@gmail.com>
2025-01-02 11:15:08 +0900

    Feat : add MongoDB integration, document load & store hooks, update editor style

--- backend-hocuspocus/src/main.ts
+++ backend-hocuspocus/src/main.ts
@@ -1,9 +1,13 @@
-import { Hocuspocus } from '@hocuspocus/server';
+import { config } from 'dotenv';
+config();
+
+// import { Hocuspocus } from '@hocuspocus/server';
+import server from './server/app';
 
 // Configure the server …
-const server = new Hocuspocus({
-  port: 8001,
-});
+// const server = new Hocuspocus({
+//   port: 8001,
+// });
 
 // … and run it!
 server.listen();
--- frontend-tiptap/src/app/[documentId]/page.tsx
+++ frontend-tiptap/src/app/[documentId]/page.tsx
@@ -13,13 +13,13 @@ export default function Home() {
   const username = query?.get('user') ?? 'John Doe';
 
   const provider = new HocuspocusProvider({
-    url: 'ws://localhost:8001',
+    url: 'http://localhost:8001',
     name: params.documentId as string,
     document: ydoc,
   });
 
   return (
-    <div>
+    <div className="bg-slate-100">
       <Tiptap provider={provider} username={username} />{' '}
     </div>
   );
